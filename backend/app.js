fd0f99ae81eb3d52cb6150933cf5620d7bde5c1f
hanan <hananzarrou@gmail.com>
2021-04-24 16:38:29 +0200

    projet go fullstack

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+
+
+//4 eme etape creer des routes et donc remplacer tout les mildwares par ceci //
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+
+//mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.5fuiw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,//
+mongoose.connect('mongodb+srv://hananzarrou:13021974hanan@cluster0.5fuiw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports=app;
+
